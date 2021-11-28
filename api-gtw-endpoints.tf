37037d6853cd75704f2a34dd57e63eaad232cd71
brendonmatos <brendonferreiradm@gmail.com>
2021-11-27 23:52:49 -0300

    chore: improving role arn

--- api-gtw-endpoints.tf
+++ api-gtw-endpoints.tf
@@ -2,11 +2,11 @@ module "aws_lambda_endpoint" {
   source = "./modules/aws-lambda-endpoint"
 
   aws_bucket_id                 = aws_s3_bucket.lambda_bucket.id
-  aws_role_arn                  = aws_iam_role.lambda_exec.arn
   aws_api_gateway_execution_arn = aws_apigatewayv2_api.this.execution_arn
   aws_api_gateway_id            = aws_apigatewayv2_api.this.id
 
   for_each = { for endpoint in var.endpoints : endpoint.path => endpoint }
+  aws_role_arn                  = coalesce(each.value.iam_role_arn, aws_iam_role.lambda_exec.arn)
 
   aws_lambda_name  = each.value.name
   http_path        = each.value.path
